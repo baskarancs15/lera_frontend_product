5c1ae1913105c05dd6dae7110759021b054d397f
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2021-01-13 12:29:37 +0530

    navbar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutPageComponent },
   { path: 'sidemenu', component: SidemenuComponent },
+  { path: 'navbar', component: NavbarMenuComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutPageComponent,
-    SidemenuComponent
+    SidemenuComponent,
+    NavbarMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    SidemenuComponent,
+    NavbarMenuComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
