bd038a2f3ff398c1e41ac3caa3d0a709ea559691
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2021-01-13 10:06:59 +0530

    about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutPageComponent },
+  { path: 'sidemenu', component: SidemenuComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
